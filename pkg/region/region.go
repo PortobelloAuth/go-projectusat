d9267b82cf80b3540c0dccc1534a9bfe2fa372e6
Aaron Madsen <amadsen@codifiant.com>
2026-07-07 18:56:44 -0600

    Update the comment in the region package to reflect states, possessions, and military address regions

--- pkg/region/region.go
+++ pkg/region/region.go
@@ -1,12 +1,73 @@
 package region
 
 /*
-Canadian Province/Territory Postal Service Abbreviation
+State or Possession Postal Abbreviations
+
+Alabama AL
+Alaska AK
+American Samoa AS
+Arizona AZ
+Arkansas AR
+California CA
+Colorado CO
+Connecticut CT
+Delaware DE
+District of Columbia DC
+Federated States of Micronesia FM
+Florida FL
+Georgia GA
+Guam GU
+Hawaii HI
+Idaho ID
+Illinois IL
+Indiana IN
+Iowa IA
+Kansas KS
+Kentucky KY
+Louisiana LA
+Maine ME
+Marshall Islands MH
+Maryland MD
+Massachusetts MA
+Michigan MI
+Minnesota MN
+Mississippi MS
+Missouri MO
+Montana MT
+Nebraska NE
+Nevada NV
+New Hampshire NH
+New Jersey NJ
+New Mexico NM
+New York NY
+North Carolina NC
+North Dakota ND
+Northern Mariana Islands MP
+Ohio OH
+Oklahoma OK
+Oregon OR
+Palau PW
+Pennsylvania PA
+Puerto Rico PR
+Rhode Island RI
+South Carolina SC
+South Dakota SD
+Tennessee TN
+Texas TX
+Utah UT
+Vermont VT
+Virgin Islands VI
+Virginia VA
+Washington WA
+West Virginia WV
+Wisconsin WI
+Wyoming WY
+
+Canadian Province/Territory Postal Service Abbreviations
+
 Alberta AB
 British Columbia BC
 Manitoba MB
-Project US@ Technical Specification
-32
 New Brunswick NB
 Newfoundland and Labrador NL
 Northwest Territories NT
@@ -17,4 +78,10 @@ Prince Edward Island PE
 Quebec QC
 Saskatchewan SK
 Yukon Territory YT
+
+Military "State" Abbreviations
+
+Armed Forces Europe, the Middle East, and Canada   AE
+Armed Forces Pacific                               AP
+Armed Forces Americas (except Canada)              AA
 */
